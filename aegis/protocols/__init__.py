f3d93b90c633a6ba265f545437e66993d181339b
Rohan   Ganesh <rohan.mudaliar@gmail.com>
2025-11-30 02:34:11 +0530

    latest working sample

--- aegis/protocols/__init__.py
+++
@@ -1,8 +0,0 @@
-"""A2A Protocol module for agent discoverability."""
-
-from aegis.protocols.a2a_protocol import (
-    create_remote_agent,
-    expose_agent_as_a2a,
-)
-
-__all__ = ["create_remote_agent", "expose_agent_as_a2a", "discover_agents"]
